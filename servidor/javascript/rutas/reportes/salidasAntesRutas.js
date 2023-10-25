d862aa8493396df78353b66216f33d7265ca7670
Juan Diego <98037053+CP-Avances@users.noreply.github.com>
2023-10-25 17:04:05 -0500

    Cambiar reporte salidas anticipadas en PDF
    Creación de los métodos para generar el reporte en formato pdf y las funciones para calcular los tiempos

--- servidor/javascript/rutas/reportes/salidasAntesRutas.js
+++ servidor/javascript/rutas/reportes/salidasAntesRutas.js
@@ -14,6 +14,8 @@ class SalidasAnticipadasRutas {
     configuracion() {
         // CONSULTA DE TIMBRES CON SALIDAS ANTICIPADAS
         this.router.put('/timbre-salida-anticipada/:desde/:hasta', verificarToken_1.TokenValidation, salidaAntesControlador_1.default.ReporteSalidasAnticipadas);
+        // CONSULTA DE TIMBRES CON SALIDAS ANTICIPADAS PARA REGIMEN CARGO
+        this.router.put('/timbre-salida-anticipada-regimen-cargo/:desde/:hasta', verificarToken_1.TokenValidation, salidaAntesControlador_1.default.ReporteSalidasAnticipadasRegimenCargo);
     }
 }
 const SALIDAS_ANTICIPADAS_RUTAS = new SalidasAnticipadasRutas();
--- servidor/src/rutas/reportes/salidasAntesRutas.ts
+++ servidor/src/rutas/reportes/salidasAntesRutas.ts
@@ -12,6 +12,9 @@ class SalidasAnticipadasRutas {
     configuracion(): void {
         // CONSULTA DE TIMBRES CON SALIDAS ANTICIPADAS
         this.router.put('/timbre-salida-anticipada/:desde/:hasta', TokenValidation, SALIDAS_ANTICIPADAS_CONTROLADOR.ReporteSalidasAnticipadas);
+        
+        // CONSULTA DE TIMBRES CON SALIDAS ANTICIPADAS PARA REGIMEN CARGO
+        this.router.put('/timbre-salida-anticipada-regimen-cargo/:desde/:hasta', TokenValidation, SALIDAS_ANTICIPADAS_CONTROLADOR.ReporteSalidasAnticipadasRegimenCargo);
 
     }
 }
