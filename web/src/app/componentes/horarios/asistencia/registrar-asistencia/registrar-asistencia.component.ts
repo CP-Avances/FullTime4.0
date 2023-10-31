815bd455249beadbb61655ab58aafaa7e68789cc
CP-Avances <jtipan@casapazmino.com.ec>
2023-10-31 17:02:07 -0500

    inicio metodo verificacion sistema

--- web/src/app/componentes/horarios/asistencia/registrar-asistencia/registrar-asistencia.component.ts
+++ web/src/app/componentes/horarios/asistencia/registrar-asistencia/registrar-asistencia.component.ts
@@ -88,27 +88,7 @@ export class RegistrarAsistenciaComponent implements OnInit {
     this.timbres = [];
     var funcion = '';
 
-    if (this.informacion.detalle.tipo_entr_salida === 'E') {
-      funcion = '0';
-    }
-    else if (this.informacion.detalle.tipo_entr_salida === 'S') {
-      funcion = '1';
-    }
-    else if (this.informacion.detalle.tipo_entr_salida === 'I/A') {
-      funcion = '2';
-    }
-    else if (this.informacion.detalle.tipo_entr_salida === 'F/A') {
-      funcion = '3';
-    }
-    else if (this.informacion.detalle.tipo_entr_salida === 'I/P') {
-      funcion = '4';
-    }
-    else if (this.informacion.detalle.tipo_entr_salida === 'F/P') {
-      funcion = '5';
-    }
-    else if (this.informacion.detalle.tipo_entr_salida === 'HA') {
-      funcion = '7';
-    }
+    funcion = this.VerificarFuncion();
 
     let datos = {
       codigo: this.informacion.detalle.codigo,
@@ -189,18 +169,92 @@ export class RegistrarAsistenciaComponent implements OnInit {
       });
       this.progreso = false;
     })
-
-
   }
   // METODO PARA CERRAR VENTANA
   CerrarVentana(opcion: number) {
     if (this.informacion.pagina === 'buscar-asistencia') {
       this.componneteb.ver_detalle = false;
       this.componneteb.ver_asistencia = true;
-      if(opcion === 2){
+      if (opcion === 2) {
         this.componneteb.BuscarDatosAsistencia(this.componneteb.formulario.value);
       }
     }
+  }
+
+  // METODO PARA ASIGNACION DESDE EL SISTEMA
+  ReasignarSistema() {
+
+    this.timbres = [];
+    var funcion = '';
+
+    funcion = this.VerificarFuncion();
+
+    console.log('ver funcion ', funcion)
+
+    /*
+    let datos = {
+      codigo: this.informacion.detalle.codigo,
+      funcion: funcion,
+      fecha: this.informacion.detalle.fecha_horario
+    }
+
+    this.timbre.BuscarTimbresAsistencia(datos).subscribe(data => {
+      console.log('ver datos ', data)
+      if (data.message === 'OK') {
+        console.log('ver respuesta ', data.respuesta)
+        this.timbres = data.respuesta;
+        this.timbres.forEach((obj: any) => {
+          //console.log('ver fecha ', moment(obj.t_fec_timbre).format('YYYY-MM-DD'))
+          //console.log('ver hora ', moment(obj.t_hora_timbre).format('HH:mm:ss'))
+          obj.fecha = this.validar.FormatearFecha(moment(obj.t_fec_timbre).format('YYYY-MM-DD'), this.formato_fecha, this.validar.dia_abreviado);
+          obj.hora = this.validar.FormatearHora(obj.t_hora_timbre, this.formato_hora);
+        })
+        this.ControlarBotones(true, true, true);
+      }
+      else {
+        this.toastr.warning('No se han encontrado registros.', '', {
+          timeOut: 6000,
+        });
+        this.ControlarBotones(false, false, false);
+      }
+    }, vacio => {
+      this.toastr.warning('No se han encontrado registros.', '', {
+        timeOut: 6000,
+      });
+      this.ControlarBotones(false, false, false);
+    })
+
+    */
+
+
+
+  }
+
+  // METODO PARA VERIFICAR FUNCION
+  VerificarFuncion() {
+    var funcion = '';
+    if (this.informacion.detalle.tipo_entr_salida === 'E') {
+      funcion = '0';
+    }
+    else if (this.informacion.detalle.tipo_entr_salida === 'S') {
+      funcion = '1';
+    }
+    else if (this.informacion.detalle.tipo_entr_salida === 'I/A') {
+      funcion = '2';
+    }
+    else if (this.informacion.detalle.tipo_entr_salida === 'F/A') {
+      funcion = '3';
+    }
+    else if (this.informacion.detalle.tipo_entr_salida === 'I/P') {
+      funcion = '4';
+    }
+    else if (this.informacion.detalle.tipo_entr_salida === 'F/P') {
+      funcion = '5';
+    }
+    else if (this.informacion.detalle.tipo_entr_salida === 'HA') {
+      funcion = '7';
+    }
+    return funcion;
   }
 
 }
