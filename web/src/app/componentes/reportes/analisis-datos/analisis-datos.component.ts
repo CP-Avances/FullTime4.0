475f7f787e882b7d85f43fb119670dde18dd3d0c
Juan Diego <98037053+CP-Avances@users.noreply.github.com>
2024-01-08 17:00:40 -0500

    Cambiar reporte analisis-datos
    Se configuro el reporte para que obtenga la url desde un parámetro del sistema

--- web/src/app/componentes/reportes/analisis-datos/analisis-datos.component.ts
+++ web/src/app/componentes/reportes/analisis-datos/analisis-datos.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+
+import { ParametrosService } from 'src/app/servicios/parametrosGenerales/parametros.service';
 
 @Component({
   selector: 'app-analisis-datos',
   templateUrl: './analisis-datos.component.html',
   styleUrls: ['./analisis-datos.component.css']
 })
-export class AnalisisDatosComponent {
+export class AnalisisDatosComponent implements OnInit{
 
+  //URL HERRAMIENTA DE ANALISIS DE DATOS
+  urlHerramienta: string;
+  urlSafe: SafeUrl;
+  cargarHerramienta: boolean = false;
+
+  constructor(private sanitized: DomSanitizer, private parametro: ParametrosService) {}
+
+  ngOnInit(): void {
+    this.BuscarURL();
+  }
+
+  //METODO PARA BUSCAR PARAMETRO DE URL DE LA HERRAMIENTA DE ANALISIS
+  BuscarURL(){
+    this.parametro.ListarDetalleParametros(3).subscribe(
+      res => {
+        this.urlHerramienta = res[0].descripcion;
+        this.urlSafe = this.sanitized.bypassSecurityTrustResourceUrl(this.urlHerramienta);
+        this.cargarHerramienta = true;
+      }
+    )
+  }
 }
