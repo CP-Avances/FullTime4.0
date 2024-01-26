c3c4a62c7c7f796a3e864c4779cac3711d85df09
Juan Diego <98037053+CP-Avances@users.noreply.github.com>
2024-01-26 11:29:54 -0500

    Crear componente cargar-plantilla-planificacion

---
+++ web/src/app/componentes/horarios/cargar-plantillas/cargar-plantilla-planificacion/cargar-plantilla-planificacion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cargar-plantilla-planificacion',
+  templateUrl: './cargar-plantilla-planificacion.component.html',
+  styleUrls: ['./cargar-plantilla-planificacion.component.css']
+})
+export class CargarPlantillaPlanificacionComponent {
+
+}
