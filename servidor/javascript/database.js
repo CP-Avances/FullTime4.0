8888d08c4925e816c221c3c144229fda66d47701
Pablo Suntaxi <pbsun.ale@gmail.com>
2024-04-01 16:38:23 -0500

    creacion de carpeta empleado

--- servidor/javascript/database.js
+++ servidor/javascript/database.js
@@ -7,8 +7,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const pg_pool_1 = __importDefault(require("pg-pool"));
 const pool = new pg_pool_1.default({
     user: 'fulltime',
-    host: '192.168.0.156',
-    port: 5432,
+    host: '186.4.226.49',
+    port: 9192,
     database: 'fulltime4_prueba',
     password: 'fulltime'
 });
