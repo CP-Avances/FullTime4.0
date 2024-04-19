0857d4408e87407b68fe055f3c89d49e4245032f
Juan Diego <98037053+CP-Avances@users.noreply.github.com>
2024-04-19 12:44:22 -0500

    Update cargaMultipleControlador.ts

--- servidor/src/controlador/cargaMultiple/cargaMultipleControlador.ts
+++ servidor/src/controlador/cargaMultiple/cargaMultipleControlador.ts
@@ -1,3 +1,4 @@
+import AUDITORIA_CONTROLADOR from '../auditoria/auditoriaControlador';
 import { Request, Response } from 'express';
 import pool from '../../database';
 import excel from 'xlsx';
@@ -6,6 +7,7 @@ import fs from 'fs';
 class CargaMultipleControlador {
 
     public async CargaMultiple(req: Request, res: Response): Promise<void> {
+        // TODO ANALIZAR COMO OBTENER DESDE EL FRONT EL USERNAME Y LA IP
         let list: any = req.files;
         let cadena = list.uploads[0].path;
         let filename = cadena.split("\\")[1];
@@ -17,38 +19,62 @@ class CargaMultipleControlador {
         const plan = excel.utils.sheet_to_json(workbook.Sheets[sheet_name_list[1]]);
         const detalle = excel.utils.sheet_to_json(workbook.Sheets[sheet_name_list[2]]);
 
-        // Recorrer por la lista de cada uno de los empleados
+        const user_name = '', ip = '';
+
+        // RECORRER POR LA LISTA DE CADA UNO DE LOS EMPLEADOS
         empleado.forEach(async (data: any) => {
             const { cedula_empleado } = data;
-            console.log('entra_empleado', cedula_empleado);
-            // Buscar el id del empleado
+
+            // BUSCAR EL ID DEL EMPLEADO
             const id_empleado = await pool.query('SELECT id FROM empleados WHERE cedula = $1', [cedula_empleado]);
-            // Buscar el id_cargo actual del empleado
+            // BUSCAR EL ID_CARGO ACTUAL DEL EMPLEADO
             const id_cargo_empleado = await pool.query('SELECT MAX(e_cargo.id) ' +
                 'FROM empl_cargos AS e_cargo, empl_contratos AS contrato_e, empleados AS e ' +
                 'WHERE contrato_e.id_empleado = e.id AND e_cargo.id_empl_contrato = contrato_e.id ' +
                 'AND e.id = $1', [id_empleado.rows[0]['id']]);
 
-            // Registrar planificacion de horario al empleado   
+            // REGISTRAR PLANIFICACION DE HORARIO AL EMPLEADO   
             plan.forEach(async (data2: any) => {
-                const { fecha_inicio, fecha_final } = data2;
-                console.log('horario', cedula_empleado, fecha_inicio, fecha_final);
-                await pool.query('INSERT INTO plan_horarios ( id_cargo, fec_inicio, fec_final ) ' +
-                    'VALUES ($1, $2, $3)', [id_cargo_empleado.rows[0]['max'], fecha_inicio, fecha_final]);
+                try {
+                    const { fecha_inicio, fecha_final } = data2;
 
-                // Registrar detalle de la planificación del horario
-                detalle.forEach(async (data3: any) => {
-                    const { fecha, tipo_dia, horario } = data3;
-                    console.log('detalle', cedula_empleado, fecha_inicio, fecha_final, fecha, tipo_dia, horario);
-                    // Buscar el id del plan registrado por empleado
-                    const id_plan = await pool.query('SELECT MAX(ph.id) FROM plan_horarios AS ph ' +
-                        'WHERE ph.id_cargo = $1', [id_cargo_empleado.rows[0]['max']]);
-                    // Buscar el id del horario ingresado
-                    const id_horario = await pool.query('SELECT id FROM cg_horarios WHERE nombre = $1', [horario]);
-                    // Registrar los detalles de la planificación del horario
-                    await pool.query('INSERT INTO plan_hora_detalles ( fecha, id_plan_horario, tipo_dia, id_cg_horarios ) ' +
-                        'VALUES ($1, $2, $3, $4)', [fecha, id_plan.rows[0]['max'], parseInt(tipo_dia.split('.-')[0]), id_horario.rows[0]['id']]);
-                });
+                    // INICIAR TRANSACCIÓN
+                    await pool.query('BEGIN');
+
+                    await pool.query('INSERT INTO plan_horarios ( id_cargo, fec_inicio, fec_final ) ' +
+                        'VALUES ($1, $2, $3)', [id_cargo_empleado.rows[0]['max'], fecha_inicio, fecha_final]);
+                    
+                    // INSERTAR AUDITORIA
+                    await AUDITORIA_CONTROLADOR.InsertarAuditoria({
+                        tabla: 'plan_horarios',
+                        usuario: user_name,
+                        accion: 'I',
+                        datosOriginales: '',
+                        datosNuevos: `{Empleado: ${cedula_empleado}, Fecha inicio: ${fecha_inicio}, Fecha final: ${fecha_final}}`,
+                        ip: ip,
+                        observacion: null
+                    });
+    
+                    // REGISTRAR DETALLE DE LA PLANIFICACIÓN DEL HORARIO
+                    detalle.forEach(async (data3: any) => {
+                        const { fecha, tipo_dia, horario } = data3;
+                        console.log('detalle', cedula_empleado, fecha_inicio, fecha_final, fecha, tipo_dia, horario);
+                        // BUSCAR EL ID DEL PLAN REGISTRADO POR EMPLEADO
+                        const id_plan = await pool.query('SELECT MAX(ph.id) FROM plan_horarios AS ph ' +
+                            'WHERE ph.id_cargo = $1', [id_cargo_empleado.rows[0]['max']]);
+                        // BUSCAR EL ID DEL HORARIO INGRESADO
+                        const id_horario = await pool.query('SELECT id FROM cg_horarios WHERE nombre = $1', [horario]);
+                        // REGISTRAR LOS DETALLES DE LA PLANIFICACIÓN DEL HORARIO
+                        await pool.query('INSERT INTO plan_hora_detalles ( fecha, id_plan_horario, tipo_dia, id_cg_horarios ) ' +
+                            'VALUES ($1, $2, $3, $4)', [fecha, id_plan.rows[0]['max'], parseInt(tipo_dia.split('.-')[0]), id_horario.rows[0]['id']]);
+                    });
+
+                    // FINALIZAR TRANSACCIÓN
+                    await pool.query('COMMIT');
+                } catch (error) {
+                    // REVERTIR TRANSACCIÓN
+                    await pool.query('ROLLBACK');           
+                }
             });
         });
         res.jsonp({ message: 'La plantilla a sido receptada' });
@@ -65,39 +91,64 @@ class CargaMultipleControlador {
 
     // **************** verificar empl_horario
     public async CargarHorarioMultiplesEmpleados(req: Request, res: Response): Promise<void> {
+        // TODO ANALIZAR COMO OBTENER DESDE EL FRONT EL USERNAME Y LA IP
         let list: any = req.files;
         let cadena = list.uploads[0].path;
         let filename = cadena.split("\\")[1];
         var filePath = `./plantillas/${filename}`
 
         const workbook = excel.readFile(filePath);
-        const sheet_name_list = workbook.SheetNames; // Array de hojas de calculo
+        const sheet_name_list = workbook.SheetNames; // ARRAY DE HOJAS DE CALCULO
         const empleados = excel.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
         const horarios = excel.utils.sheet_to_json(workbook.Sheets[sheet_name_list[1]]);
 
-        // Arreglo de empleados
+        const user_name = '', ip = '';
+
+        // ARREGLO DE EMPLEADOS
         empleados.forEach(async (data: any) => {
-            var { cedula } = data;
-            // Recorrer por todos los empleados y buscar el id del empleado
-            const id_empleado = await pool.query('SELECT id FROM empleados WHERE cedula = $1', [cedula]);
-            // Buscar el id_cargo actual del empleado
-            const id_cargo_empleado = await pool.query('SELECT MAX(e_cargo.id) ' +
-                'FROM empl_cargos AS e_cargo, empl_contratos AS contrato_e, empleados AS e ' +
-                'WHERE contrato_e.id_empleado = e.id AND e_cargo.id_empl_contrato = contrato_e.id ' +
-                'AND e.id = $1', [id_empleado.rows[0]['id']]);
-            // Arreglo de horario fijo
-            horarios.forEach(async (data: any) => {
-                var { fecha_inicio, fecha_final, lunes, martes, miercoles, jueves, viernes, sabado, domingo, nombre_horario, estado } = data;
-                // Buscar el id del horario ingresado
-                const id_horario = await pool.query('SELECT id FROM cg_horarios WHERE nombre = $1', [nombre_horario]);
-                var id_hora = 1;
-                // Registrar los datos del horario fijo de varios empleados
-                await pool.query('INSERT INTO empl_horarios (id_empl_cargo, id_hora, fec_inicio, fec_final, ' +
-                    'lunes, martes, miercoles, jueves, viernes, sabado, domingo, id_horarios, estado) ' +
-                    'VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)',
-                    [id_cargo_empleado.rows[0]['max'], id_hora, fecha_inicio, fecha_final, lunes, martes, miercoles, jueves, viernes, sabado, domingo, id_horario.rows[0]['id'], estado.split("-")[0]]);
-                console.log('Registro exitoso');
-            });
+            try {
+                var { cedula } = data;
+                // RECORRER POR TODOS LOS EMPLEADOS Y BUSCAR EL ID DEL EMPLEADO
+                const id_empleado = await pool.query('SELECT id FROM empleados WHERE cedula = $1', [cedula]);
+                // BUSCAR EL ID_CARGO ACTUAL DEL EMPLEADO
+                const id_cargo_empleado = await pool.query('SELECT MAX(e_cargo.id) ' +
+                    'FROM empl_cargos AS e_cargo, empl_contratos AS contrato_e, empleados AS e ' +
+                    'WHERE contrato_e.id_empleado = e.id AND e_cargo.id_empl_contrato = contrato_e.id ' +
+                    'AND e.id = $1', [id_empleado.rows[0]['id']]);
+                // ARREGLO DE HORARIO FIJO
+                horarios.forEach(async (data: any) => {
+                    var { fecha_inicio, fecha_final, lunes, martes, miercoles, jueves, viernes, sabado, domingo, nombre_horario, estado } = data;
+                    // BUSCAR EL ID DEL HORARIO INGRESADO
+                    const id_horario = await pool.query('SELECT id FROM cg_horarios WHERE nombre = $1', [nombre_horario]);
+                    var id_hora = 1;
+
+                    // INICIAR TRANSACCIÓN
+                    await pool.query('BEGIN');
+
+                    // REGISTRAR LOS DATOS DEL HORARIO FIJO DE VARIOS EMPLEADOS
+                    await pool.query('INSERT INTO empl_horarios (id_empl_cargo, id_hora, fec_inicio, fec_final, ' +
+                        'lunes, martes, miercoles, jueves, viernes, sabado, domingo, id_horarios, estado) ' +
+                        'VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)',
+                        [id_cargo_empleado.rows[0]['max'], id_hora, fecha_inicio, fecha_final, lunes, martes, miercoles, jueves, viernes, sabado, domingo, id_horario.rows[0]['id'], estado.split("-")[0]]);
+                    
+                    // INSERTAR AUDITORIA
+                    await AUDITORIA_CONTROLADOR.InsertarAuditoria({
+                        tabla: 'empl_horarios',
+                        usuario: user_name,
+                        accion: 'I',
+                        datosOriginales: '',
+                        datosNuevos: `{Empleado: ${cedula}, Fecha inicio: ${fecha_inicio}, Fecha final: ${fecha_final}, lunes: ${lunes}, martes: ${martes}, miercoles: ${miercoles}, jueves: ${jueves}, viernes: ${viernes}, sabado: ${sabado}, domingo: ${domingo}, Horario: ${nombre_horario}, Estado: ${estado}}`,
+                        ip: ip,
+                        observacion: null
+                    });
+
+                    // FINALIZAR TRANSACCIÓN
+                    await pool.query('COMMIT');
+                });
+            } catch (error) {
+                // REVERTIR TRANSACCIÓN
+                await pool.query('ROLLBACK');    
+            }
         });
         res.jsonp({ message: 'La plantilla a sido receptada' });
         
