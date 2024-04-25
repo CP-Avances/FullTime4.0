9f248b5275f4c7f44a226ededdb1e0cb91db30a5
Juan Diego <98037053+CP-Avances@users.noreply.github.com>
2024-04-25 10:02:10 -0500

    Update catTipoComidasControlador.ts
    Implementación de auditoria en los métodos del controlador
    Implementación de transacciones

--- servidor/src/controlador/catalogos/catTipoComidasControlador.ts
+++ servidor/src/controlador/catalogos/catTipoComidasControlador.ts
@@ -1,146 +1,394 @@
-import { Request, Response } from 'express';
-import { QueryResult } from 'pg';
-import fs from 'fs';
-const builder = require('xmlbuilder');
+import { Request, Response } from "express";
+import { QueryResult } from "pg";
+import AUDITORIA_CONTROLADOR from "../auditoria/auditoriaControlador";
 
-import pool from '../../database';
+import pool from "../../database";
 
 class TipoComidasControlador {
-
-    public async ListarTipoComidas(req: Request, res: Response) {
-        const TIPO_COMIDAS = await pool.query('SELECT ctc.id, ctc.nombre, ctc.tipo_comida, ctc.hora_inicio, ' +
-            'ctc.hora_fin, tc.nombre AS tipo FROM cg_tipo_comidas AS ctc, tipo_comida AS tc ' +
-            'WHERE ctc.tipo_comida = tc.id ORDER BY tc.nombre ASC, ctc.id ASC');
-        if (TIPO_COMIDAS.rowCount > 0) {
-            return res.jsonp(TIPO_COMIDAS.rows)
-        }
-        else {
-            return res.status(404).jsonp({ text: 'No se encuentran registros' });
-        }
+  public async ListarTipoComidas(req: Request, res: Response) {
+    const TIPO_COMIDAS = await pool.query(
+      "SELECT ctc.id, ctc.nombre, ctc.tipo_comida, ctc.hora_inicio, " +
+        "ctc.hora_fin, tc.nombre AS tipo FROM cg_tipo_comidas AS ctc, tipo_comida AS tc " +
+        "WHERE ctc.tipo_comida = tc.id ORDER BY tc.nombre ASC, ctc.id ASC"
+    );
+    if (TIPO_COMIDAS.rowCount > 0) {
+      return res.jsonp(TIPO_COMIDAS.rows);
+    } else {
+      return res.status(404).jsonp({ text: "No se encuentran registros" });
     }
+  }
 
-    public async ListarTipoComidasDetalles(req: Request, res: Response) {
-        const TIPO_COMIDAS = await pool.query('SELECT ctc.id, ctc.nombre, ctc.tipo_comida, ctc.hora_inicio, ' +
-            'ctc.hora_fin, tc.nombre AS tipo, dm.nombre AS nombre_plato, dm.valor, dm.observacion ' +
-            'FROM cg_tipo_comidas AS ctc, tipo_comida AS tc, detalle_menu AS dm ' +
-            'WHERE ctc.tipo_comida = tc.id AND dm.id_menu = ctc.id ORDER BY tc.nombre ASC, ctc.id ASC');
-        if (TIPO_COMIDAS.rowCount > 0) {
-            return res.jsonp(TIPO_COMIDAS.rows)
-        }
-        else {
-            return res.status(404).jsonp({ text: 'No se encuentran registros' });
-        }
+  public async ListarTipoComidasDetalles(req: Request, res: Response) {
+    const TIPO_COMIDAS = await pool.query(
+      "SELECT ctc.id, ctc.nombre, ctc.tipo_comida, ctc.hora_inicio, " +
+        "ctc.hora_fin, tc.nombre AS tipo, dm.nombre AS nombre_plato, dm.valor, dm.observacion " +
+        "FROM cg_tipo_comidas AS ctc, tipo_comida AS tc, detalle_menu AS dm " +
+        "WHERE ctc.tipo_comida = tc.id AND dm.id_menu = ctc.id ORDER BY tc.nombre ASC, ctc.id ASC"
+    );
+    if (TIPO_COMIDAS.rowCount > 0) {
+      return res.jsonp(TIPO_COMIDAS.rows);
+    } else {
+      return res.status(404).jsonp({ text: "No se encuentran registros" });
     }
+  }
 
-    public async VerUnMenu(req: Request, res: Response): Promise<any> {
-        const { id } = req.params;
-        const TIPO_COMIDAS = await pool.query('SELECT ctc.id, ctc.nombre, ctc.tipo_comida, ctc.hora_inicio, ' +
-            'ctc.hora_fin, tc.nombre AS tipo FROM cg_tipo_comidas AS ctc, tipo_comida AS tc ' +
-            'WHERE ctc.tipo_comida = tc.id AND ctc.id = $1', [id]);
-        if (TIPO_COMIDAS.rowCount > 0) {
-            return res.jsonp(TIPO_COMIDAS.rows)
-        }
-        else {
-            return res.status(404).jsonp({ text: 'No se encuentran registros' });
-        }
+  public async VerUnMenu(req: Request, res: Response): Promise<any> {
+    const { id } = req.params;
+    const TIPO_COMIDAS = await pool.query(
+      "SELECT ctc.id, ctc.nombre, ctc.tipo_comida, ctc.hora_inicio, " +
+        "ctc.hora_fin, tc.nombre AS tipo FROM cg_tipo_comidas AS ctc, tipo_comida AS tc " +
+        "WHERE ctc.tipo_comida = tc.id AND ctc.id = $1",
+      [id]
+    );
+    if (TIPO_COMIDAS.rowCount > 0) {
+      return res.jsonp(TIPO_COMIDAS.rows);
+    } else {
+      return res.status(404).jsonp({ text: "No se encuentran registros" });
     }
+  }
 
-    public async ListarUnTipoComida(req: Request, res: Response): Promise<any> {
-        const { id } = req.params;
-        const TIPO_COMIDAS = await pool.query('SELECT ctc.id, ctc.nombre, ctc.tipo_comida, ctc.hora_inicio, ' +
-            'ctc.hora_fin, tc.nombre AS tipo FROM cg_tipo_comidas AS ctc, tipo_comida AS tc ' +
-            ' WHERE ctc.tipo_comida = tc.id AND tc.id = $1 ORDER BY tc.nombre ASC', [id]);
-        if (TIPO_COMIDAS.rowCount > 0) {
-            return res.jsonp(TIPO_COMIDAS.rows)
-        }
-        else {
-            return res.status(404).jsonp({ text: 'No se encuentran registros' });
-        }
+  public async ListarUnTipoComida(req: Request, res: Response): Promise<any> {
+    const { id } = req.params;
+    const TIPO_COMIDAS = await pool.query(
+      "SELECT ctc.id, ctc.nombre, ctc.tipo_comida, ctc.hora_inicio, " +
+        "ctc.hora_fin, tc.nombre AS tipo FROM cg_tipo_comidas AS ctc, tipo_comida AS tc " +
+        " WHERE ctc.tipo_comida = tc.id AND tc.id = $1 ORDER BY tc.nombre ASC",
+      [id]
+    );
+    if (TIPO_COMIDAS.rowCount > 0) {
+      return res.jsonp(TIPO_COMIDAS.rows);
+    } else {
+      return res.status(404).jsonp({ text: "No se encuentran registros" });
     }
+  }
 
-    public async CrearTipoComidas(req: Request, res: Response) {
+  public async CrearTipoComidas(req: Request, res: Response) {
+    try {
+      const { nombre, tipo_comida, hora_inicio, hora_fin, user_name, ip } = req.body;
 
-        const { nombre, tipo_comida, hora_inicio, hora_fin } = req.body;
+      // INICIAR TRANSACCION
+      await pool.query("BEGIN");
+  
+      const response: QueryResult = await pool.query(
+        `
+              INSERT INTO cg_tipo_comidas (nombre, tipo_comida, hora_inicio, hora_fin)
+              VALUES ($1, $2, $3, $4) RETURNING *
+              `,
+        [nombre, tipo_comida, hora_inicio, hora_fin]
+      );
+  
+      const [tipos_comida] = response.rows;
 
-        const response: QueryResult = await pool.query(
-            `
-            INSERT INTO cg_tipo_comidas (nombre, tipo_comida, hora_inicio, hora_fin)
-            VALUES ($1, $2, $3, $4) RETURNING *
-            `
-            , [nombre, tipo_comida, hora_inicio, hora_fin]);
+      // AUDITORIA
+      await AUDITORIA_CONTROLADOR.InsertarAuditoria({
+        tabla: "cg_tipo_comidas",
+        usuario: user_name,
+        accion: "I",
+        datosOriginales: "",
+        datosNuevos: `{nombre: ${nombre}, tipo_comida: ${tipo_comida}, hora_inicio: ${hora_inicio}, hora_fin: ${hora_fin}}`,
+        ip,
+        observacion: null,
+      });
+  
+      // FINALIZAR TRANSACCION
+      await pool.query("COMMIT");
 
-        const [tipos_comida] = response.rows;
-
-        if (!tipos_comida) {
-            return res.status(404).jsonp({ message: 'error' })
-        }
-        else {
-            return res.status(200).jsonp({ message: 'OK', info: tipos_comida });
-        }
+      if (!tipos_comida) {
+        return res.status(404).jsonp({ message: "error" });
+      } else {
+        return res.status(200).jsonp({ message: "OK", info: tipos_comida });
+      }
+    } catch (error) {
+      // REVERTIR TRANSACCION
+      await pool.query("ROLLBACK");
+      res.status(404).jsonp({ message: "Error al guardar el registro." });
     }
+  }
 
-    public async ActualizarComida(req: Request, res: Response): Promise<void> {
-        const { nombre, tipo_comida, hora_inicio, hora_fin, id } = req.body;
-        await pool.query('UPDATE cg_tipo_comidas SET nombre = $1, tipo_comida = $2, hora_inicio = $3, hora_fin = $4 ' +
-            'WHERE id = $5', [nombre, tipo_comida, hora_inicio, hora_fin, id]);
-        res.jsonp({ message: 'Registro actualizado exitosamente' });
+  public async ActualizarComida(req: Request, res: Response): Promise<Response> {
+    try {
+      const { nombre, tipo_comida, hora_inicio, hora_fin, id, user_name, ip } = req.body;
+      
+      // INICIAR TRANSACCION
+      await pool.query("BEGIN");
+
+      // CONSULTAR DATOSORIGINALES
+      const datosOriginales = await pool.query("SELECT * FROM cg_tipo_comidas WHERE id = $1", [id]);
+      const [datos] = datosOriginales.rows;
+
+      if (!datos) {
+        // AUDITORIA
+        await AUDITORIA_CONTROLADOR.InsertarAuditoria({
+          tabla: "cg_tipo_comidas",
+          usuario: user_name,
+          accion: "U",
+          datosOriginales: "",
+          datosNuevos: "",
+          ip,
+          observacion: `Error al actualizar el registro con id ${id}`,
+        });
+
+        // FINALIZAR TRANSACCION
+        await pool.query("COMMIT");
+        return res.status(404).jsonp({ message: "error" });
+      }
+      
+      await pool.query(
+        "UPDATE cg_tipo_comidas SET nombre = $1, tipo_comida = $2, hora_inicio = $3, hora_fin = $4 " +
+          "WHERE id = $5",
+        [nombre, tipo_comida, hora_inicio, hora_fin, id]
+      );
+
+      // AUDITORIA
+      await AUDITORIA_CONTROLADOR.InsertarAuditoria({
+        tabla: "cg_tipo_comidas",
+        usuario: user_name,
+        accion: "U",
+        datosOriginales: JSON.stringify(datos),
+        datosNuevos: `{nombre: ${nombre}, tipo_comida: ${tipo_comida}, hora_inicio: ${hora_inicio}, hora_fin: ${hora_fin}}`,
+        ip,
+        observacion: null,
+      });
+
+      // FINALIZAR TRANSACCION
+      await pool.query("COMMIT");
+      return res.jsonp({ message: "Registro actualizado exitosamente" });
+    } catch (error) {
+      // REVERTIR TRANSACCION
+      await pool.query("ROLLBACK");
+      return res.status(404).jsonp({ message: "Error al actualizar el registro." });
     }
+  }
 
-    public async EliminarRegistros(req: Request, res: Response): Promise<void> {
-        const id = req.params.id;
-        await pool.query('DELETE FROM cg_tipo_comidas WHERE id = $1', [id]);
-        res.jsonp({ message: 'Registro eliminado.' });
+  public async EliminarRegistros(req: Request, res: Response): Promise<Response> {
+    try {
+      // TODO ANALIZAR COMO OBTENER USER_NAME E ID DESDE EL FRONT
+      const { user_name, ip } = req.body;
+      const id = req.params.id;
+
+      // INICIAR TRANSACCION
+      await pool.query("BEGIN");
+
+      // CONSULTAR DATOSORIGINALES
+      const datosOriginales = await pool.query("SELECT * FROM cg_tipo_comidas WHERE id = $1", [id]);
+      const [datos] = datosOriginales.rows;
+
+      if (!datos) {
+        // AUDITORIA
+        await AUDITORIA_CONTROLADOR.InsertarAuditoria({
+          tabla: "cg_tipo_comidas",
+          usuario: user_name,
+          accion: "D",
+          datosOriginales: "",
+          datosNuevos: "",
+          ip,
+          observacion: `Error al eliminar el registro con id ${id}`,
+        });
+
+        // FINALIZAR TRANSACCION
+        await pool.query("COMMIT");
+        return res.status(404).jsonp({ message: "error" });
+      }
+
+      await pool.query("DELETE FROM cg_tipo_comidas WHERE id = $1", [id]);
+
+      // AUDITORIA
+      await AUDITORIA_CONTROLADOR.InsertarAuditoria({
+        tabla: "cg_tipo_comidas",
+        usuario: user_name,
+        accion: "D",
+        datosOriginales: JSON.stringify(datos),
+        datosNuevos: "",
+        ip,
+        observacion: null,
+      });
+
+      // FINALIZAR TRANSACCION
+      await pool.query("COMMIT");
+      return res.jsonp({ message: "Registro eliminado." });
+    } catch (error) {
+      // REVERTIR TRANSACCION
+      await pool.query("ROLLBACK");
+      return res.status(404).jsonp({ message: "Error al eliminar el registro." });
     }
+  }
 
-    public async VerUltimoRegistro(req: Request, res: Response) {
-        const TIPO_COMIDAS = await pool.query('SELECT MAX (id) FROM cg_tipo_comidas');
-        if (TIPO_COMIDAS.rowCount > 0) {
-            return res.jsonp(TIPO_COMIDAS.rows)
-        }
-        else {
-            return res.status(404).jsonp({ text: 'No se encuentran registros' });
-        }
+  public async VerUltimoRegistro(req: Request, res: Response) {
+    const TIPO_COMIDAS = await pool.query(
+      "SELECT MAX (id) FROM cg_tipo_comidas"
+    );
+    if (TIPO_COMIDAS.rowCount > 0) {
+      return res.jsonp(TIPO_COMIDAS.rows);
+    } else {
+      return res.status(404).jsonp({ text: "No se encuentran registros" });
     }
+  }
 
+  // Registro de detalle de menú - desglose de platos
+  public async CrearDetalleMenu(req: Request, res: Response): Promise<void> {
+    try {
+      const { nombre, valor, observacion, id_menu, user_name, ip } = req.body;
 
-    // Registro de detalle de menú - desglose de platos
-    public async CrearDetalleMenu(req: Request, res: Response): Promise<void> {
-        const { nombre, valor, observacion, id_menu } = req.body;
-        await pool.query('INSERT INTO detalle_menu (nombre, valor, observacion, id_menu) ' +
-            'VALUES ($1, $2, $3, $4)',
-            [nombre, valor, observacion, id_menu]);
-        res.jsonp({ message: 'Detalle de menú registrada' });
+      // INICIAR TRANSACCION
+      await pool.query("BEGIN");
+
+      await pool.query(
+        "INSERT INTO detalle_menu (nombre, valor, observacion, id_menu) " +
+          "VALUES ($1, $2, $3, $4)",
+        [nombre, valor, observacion, id_menu]
+      );
+
+      // AUDITORIA
+      await AUDITORIA_CONTROLADOR.InsertarAuditoria({
+        tabla: "detalle_menu",
+        usuario: user_name,
+        accion: "I",
+        datosOriginales: "",
+        datosNuevos: `{nombre: ${nombre}, valor: ${valor}, observacion: ${observacion}, id_menu: ${id_menu}}`,
+        ip,
+        observacion: null,
+      });
+
+      // FINALIZAR TRANSACCION
+      await pool.query("COMMIT");
+      res.jsonp({ message: "Detalle de menú registrada" });
+    } catch (error) {
+      // REVERTIR TRANSACCION
+      await pool.query("ROLLBACK");
+      res.status(404).jsonp({ message: "Error al guardar el detalle de menú." });
     }
+  }
 
-    public async VerUnDetalleMenu(req: Request, res: Response): Promise<any> {
-        const { id } = req.params;
-        const TIPO_COMIDAS = await pool.query('SELECT tc.id AS id_servicio, tc.nombre AS servicio, ' +
-            'menu.id AS id_menu, menu.nombre AS menu, dm.id AS id_detalle, dm.nombre AS plato, dm.valor, ' +
-            'dm.observacion, menu.hora_inicio, menu.hora_fin ' +
-            'FROM tipo_comida AS tc, cg_tipo_comidas AS menu, detalle_menu AS dm ' +
-            'WHERE tc.id = menu.tipo_comida AND dm.id_menu = menu.id AND menu.id = $1', [id]);
-        if (TIPO_COMIDAS.rowCount > 0) {
-            return res.jsonp(TIPO_COMIDAS.rows)
-        }
-        else {
-            return res.status(404).jsonp({ text: 'No se encuentran registros' });
-        }
+  public async VerUnDetalleMenu(req: Request, res: Response): Promise<any> {
+    const { id } = req.params;
+    const TIPO_COMIDAS = await pool.query(
+      "SELECT tc.id AS id_servicio, tc.nombre AS servicio, " +
+        "menu.id AS id_menu, menu.nombre AS menu, dm.id AS id_detalle, dm.nombre AS plato, dm.valor, " +
+        "dm.observacion, menu.hora_inicio, menu.hora_fin " +
+        "FROM tipo_comida AS tc, cg_tipo_comidas AS menu, detalle_menu AS dm " +
+        "WHERE tc.id = menu.tipo_comida AND dm.id_menu = menu.id AND menu.id = $1",
+      [id]
+    );
+    if (TIPO_COMIDAS.rowCount > 0) {
+      return res.jsonp(TIPO_COMIDAS.rows);
+    } else {
+      return res.status(404).jsonp({ text: "No se encuentran registros" });
     }
+  }
 
-    public async ActualizarDetalleMenu(req: Request, res: Response): Promise<void> {
-        const { nombre, valor, observacion, id } = req.body;
-        await pool.query('UPDATE detalle_menu SET nombre = $1, valor = $2, observacion = $3 ' +
-            'WHERE id = $4',
-            [nombre, valor, observacion, id]);
-        res.jsonp({ message: 'Detalle de menú actualizado' });
+  public async ActualizarDetalleMenu(
+    req: Request,
+    res: Response
+  ): Promise<Response> {
+    try {
+      const { nombre, valor, observacion, id, user_name, ip } = req.body;
+
+      // INICIAR TRANSACCION
+      await pool.query("BEGIN");
+
+      // CONSULTAR DATOSORIGINALES
+      const datosOriginales = await pool.query(
+        "SELECT * FROM detalle_menu WHERE id = $1",
+        [id]
+      );
+      const [datos] = datosOriginales.rows;
+
+      if (!datos) {
+        // AUDITORIA
+        await AUDITORIA_CONTROLADOR.InsertarAuditoria({
+          tabla: "detalle_menu",
+          usuario: user_name,
+          accion: "U",
+          datosOriginales: "",
+          datosNuevos: "",
+          ip,
+          observacion: `Error al actualizar el registro con id ${id}`,
+        });
+
+        // FINALIZAR TRANSACCION
+        await pool.query("COMMIT");
+        return res.status(404).jsonp({ message: "error" });
+      }
+
+      await pool.query(
+        "UPDATE detalle_menu SET nombre = $1, valor = $2, observacion = $3 " +
+          "WHERE id = $4",
+        [nombre, valor, observacion, id]
+      );
+
+      // AUDITORIA
+      await AUDITORIA_CONTROLADOR.InsertarAuditoria({
+        tabla: "detalle_menu",
+        usuario: user_name,
+        accion: "U",
+        datosOriginales: JSON.stringify(datos),
+        datosNuevos: `{nombre: ${nombre}, valor: ${valor}, observacion: ${observacion}}`,
+        ip,
+        observacion: null,
+      });
+
+      // FINALIZAR TRANSACCION
+      await pool.query("COMMIT");
+      return res.jsonp({ message: "Detalle de menú actualizado" });
+    } catch (error) {
+      // REVERTIR TRANSACCION
+      await pool.query("ROLLBACK");
+      return res.status(404).jsonp({ message: "Error al actualizar el registro." });
     }
+  }
 
-    public async EliminarDetalle(req: Request, res: Response): Promise<void> {
-        const id = req.params.id;
-        await pool.query('DELETE FROM detalle_menu WHERE id = $1', [id]);
-        res.jsonp({ message: 'Registro eliminado.' });
+  public async EliminarDetalle(req: Request, res: Response): Promise<Response> {
+    try {
+      // TODO ANALIZAR COMO OBTENER USER_NAME E ID DESDE EL FRONT
+      const { user_name, ip } = req.body;
+      const id = req.params.id;
+
+      // INICIAR TRANSACCION
+      await pool.query("BEGIN");
+
+      // CONSULTAR DATOSORIGINALES
+      const datosOriginales = await pool.query(
+        "SELECT * FROM detalle_menu WHERE id = $1",
+        [id]
+      );
+      const [datos] = datosOriginales.rows;
+
+      if (!datos) {
+        // AUDITORIA
+        await AUDITORIA_CONTROLADOR.InsertarAuditoria({
+          tabla: "detalle_menu",
+          usuario: user_name,
+          accion: "D",
+          datosOriginales: "",
+          datosNuevos: "",
+          ip,
+          observacion: `Error al eliminar el registro con id ${id}`,
+        });
+
+        // FINALIZAR TRANSACCION
+        await pool.query("COMMIT");
+        return res.status(404).jsonp({ message: "error" });
+      }
+
+      await pool.query("DELETE FROM detalle_menu WHERE id = $1", [id]);
+
+      // AUDITORIA
+      await AUDITORIA_CONTROLADOR.InsertarAuditoria({
+        tabla: "detalle_menu",
+        usuario: user_name,
+        accion: "D",
+        datosOriginales: JSON.stringify(datos),
+        datosNuevos: "",
+        ip,
+        observacion: null,
+      });
+      return res.jsonp({ message: "Registro eliminado." });
+    } catch (error) {
+      // REVERTIR TRANSACCION
+      await pool.query("ROLLBACK");
+      return res.status(404).jsonp({ message: "Error al eliminar el registro." });
     }
-
+  }
 }
 
 const TIPO_COMIDAS_CONTROLADOR = new TipoComidasControlador();
