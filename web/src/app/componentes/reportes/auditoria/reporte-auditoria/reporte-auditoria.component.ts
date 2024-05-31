ee35f02f9f7cad2b90eb9c0bd2b1a0c983857e79
CP-Avances <jtipan@casapazmino.com.ec>
2024-05-31 17:04:14 -0500

    cambios pagina auditoria

--- web/src/app/componentes/reportes/auditoria/reporte-auditoria/reporte-auditoria.component.ts
+++ web/src/app/componentes/reportes/auditoria/reporte-auditoria/reporte-auditoria.component.ts
@@ -44,6 +44,10 @@ export class ReporteAuditoriaComponent implements OnInit, OnDestroy {
     // CRITERIOS DE BUSQUEDA POR FECHAS
     get rangoFechas() { return this.reporteService.rangoFechas };
 
+    // SELECCIÓN DE BUSQUEDA DE DATOS SEGÚN OPCIÓN
+    //get bool() { return this.reporteService.criteriosBusqueda };
+
+
 
     tablasSolicitadas: any = [];
     // ITEMS DE PAGINACION DE LA TABLA
@@ -145,56 +149,115 @@ export class ReporteAuditoriaComponent implements OnInit, OnDestroy {
         private reporteService: ReportesService,
         private toastr: ToastrService,
         private restAuditoria: AuditoriaService,
+        private restEmpre: EmpresaService,
 
-    ) { }
+
+    ) {
+
+        this.ObtenerLogo();
+        this.ObtenerColores();
+
+    }
 
 
     // VALIDACIONES DE OPCIONES DE REPORTE
     ValidarReporte() {
         if (this.rangoFechas.fec_inico === '' || this.rangoFechas.fec_final === '') return this.toastr.error('Primero valide fechas de búsqueda.');
-        this.BuscarTablasAuditoria();
+        this.ModelarTablasAuditoria();
     }
 
 
     //BUSCAR REGISTROS AUDITORIA
 
-    BuscarTablasAuditoria() {
+    ModelarTablasAuditoria() {
 
         this.data_pdf = [];
 
         // Crear un array de promesas para todas las consultas
-        const consultas = this.tablasSolicitadas.map(x => {
+        const consultas1 = this.tablasSolicitadas.map(x => {
             const buscarTabla = {
                 tabla: x.nombre,
                 desde: this.rangoFechas.fec_inico,
                 hasta: this.rangoFechas.fec_final,
                 action: 'I'
             };
-            // return new Promise((resolve, reject) => {
-            this.restAuditoria.ConsultarAuditoria(buscarTabla).subscribe(
-                res => {
-
-
-                    this.data_pdf.push(res)
-                    console.log(this.data_pdf);
-                }, error => {
-                    if (error.status == '404') {
-
-                        this.toastr.error('No existen registros en' + x.nombre, 'Ups!!! algo salio mal..', {
-                            timeOut: 6000,
-                        })
+            return new Promise((resolve, reject) => {
+                this.restAuditoria.ConsultarAuditoria(buscarTabla).subscribe(
+                    res => resolve(res), // Resuelve la promesa con el resultado
+                    error => {
+                        if (error.status == '404') {
+                            this.toastr.error('No existen registros en' + x.nombre, 'Ups!!! algo salio mal..', {
+                                timeOut: 6000,
+                            })
+                        }
                     }
-
-                }
-            )
+                );
+            });
         });
+
+        // Esperar a que todas las promesas se completen
+        Promise.all(consultas1)
+            .then(resultados => {
+                // Todos los resultados están disponibles aquí
+                this.data_pdf = resultados;
+                // Puedes hacer más cosas aquí después de obtener todos los resultados
+                this.GenerarPDF( this.data_pdf);
+                console.log(this.data_pdf[0][0].action)
+
+            })
+            .catch(error => {
+                // Manejar errores aquí
+                console.error('Error en una o más consultas:', error);
+            });
+
+
+        /////////////////////////////////////////
+
+        /*
+        this.data_pdf = [];
+                // Crear un array de promesas para todas las consultas
+                const consultas = this.tablasSolicitadas.map(x => {
+                    const buscarTabla = {
+                        tabla: x.nombre,
+                        desde: this.rangoFechas.fec_inico,
+                        hasta: this.rangoFechas.fec_final,
+                        action: 'I'
+                    };
+                    // return new Promise((resolve, reject) => {
+        
+        
+        
+                    this.restAuditoria.ConsultarAuditoria(buscarTabla).subscribe(
+                        res => {
+                            this.data_pdf.push(res)
+                            console.log(this.data_pdf);
+        
+        
+        
+        
+                        }, error => {
+                            if (error.status == '404') {
+                                this.toastr.error('No existen registros en' + x.nombre, 'Ups!!! algo salio mal..', {
+                                    timeOut: 6000,
+                                })
+                            }
+                        }
+                    )
+        
+        
+                });
+        
+                this.GenerarPDF();
+        
+                */
+
     }
 
 
 
 
     ngOnDestroy(): void {
-        this.BuscarTablasAuditoria();
+        //this.ModelarTablasAuditoria();
     }
     ngOnInit(): void {
     }
@@ -252,6 +315,272 @@ export class ReporteAuditoriaComponent implements OnInit, OnDestroy {
         return `${this.selectionAuditoria.isSelected(row) ? 'deselect' : 'select'} row ${row.nombre + 1}`;
 
     }
+
+    /** ****************************************************************************************** **
+   **                              COLORES Y LOGO PARA EL REPORTE                                **
+   ** ****************************************************************************************** **/
+
+    logo: any = String;
+    ObtenerLogo() {
+        this.restEmpre.LogoEmpresaImagenBase64(localStorage.getItem('empresa') as string).subscribe(res => {
+            this.logo = 'data:image/jpeg;base64,' + res.imagen;
+        });
+    }
+
+    // METODO PARA OBTENER COLORES Y MARCA DE AGUA DE EMPRESA
+    p_color: any;
+    s_color: any;
+    frase: any;
+    ObtenerColores() {
+        this.restEmpre.ConsultarDatosEmpresa(parseInt(localStorage.getItem('empresa') as string)).subscribe(res => {
+            this.p_color = res[0].color_principal;
+            this.s_color = res[0].color_secundario;
+            this.frase = res[0].marca_agua;
+        });
+    }
+
+
+
+
+    /** ****************************************************************************************** **
+ **                                              PDF                                           **
+ ** ****************************************************************************************** **/
+
+    GenerarPDF(data: any) {
+        let documentDefinition: any;
+        documentDefinition = this.GetDocumentDefinicion(data);
+        let doc_name = `Auditoría.pdf`;
+        pdfMake.createPdf(documentDefinition).open();
+
+    }
+
+
+
+    GetDocumentDefinicion(data: any) {
+        return {
+            pageSize: 'A4',
+            pageOrientation: 'portrait',
+            pageMargins: [40, 50, 40, 50],
+            watermark: { text: this.frase, color: 'blue', opacity: 0.1, bold: true, italics: false },
+            header: { text: 'Impreso por:  ' + localStorage.getItem('fullname_print'), margin: 10, fontSize: 9, opacity: 0.3, alignment: 'right' },
+
+
+            footer: function (currentPage: any, pageCount: any, fecha: any) {
+                let f = moment();
+                fecha = f.format('YYYY-MM-DD');
+                let time = f.format('HH:mm:ss');
+                return {
+                    margin: 10,
+                    columns: [
+                        { text: 'Fecha: ' + fecha + ' Hora: ' + time, opacity: 0.3 },
+                        {
+                            text: [
+                                {
+                                    text: '© Pag ' + currentPage.toString() + ' de ' + pageCount,
+                                    alignment: 'right', opacity: 0.3
+                                }
+                            ],
+                        }
+                    ],
+                    fontSize: 10
+                }
+            },
+            content: [
+                { image: this.logo, width: 100, margin: [10, -25, 0, 5] },
+                { text: (localStorage.getItem('name_empresa') as string).toUpperCase(), bold: true, fontSize: 14, alignment: 'center', margin: [0, -30, 0, 5] },
+                //{ text: (localStorage.getItem('name_empresa') as string).toUpperCase(), bold: true, fontSize: 14, alignment: 'center', margin: [0, -30, 0, 5] },
+                { text: `AUDITORÍA`, bold: true, fontSize: 12, alignment: 'center', margin: [0, 0, 0, 0] },
+                //{ text: 'PERIODO DEL: ' + this.rangoFechas.fec_inico + " AL " + this.rangoFechas.fec_final, bold: true, fontSize: 11, alignment: 'center', margin: [0, 0, 0, 0] },
+                ...this.EstructurarDatosPDF(data).map((obj: any) => {
+                    return obj
+                })
+            ],
+            styles: {
+                tableHeader: { fontSize: 8, bold: true, alignment: 'center', fillColor: this.p_color },
+                centrado: { fontSize: 8, bold: true, alignment: 'center', fillColor: this.p_color, margin: [0, 7, 0, 0] },
+                itemsTable: { fontSize: 8 },
+                itemsTableInfo: { fontSize: 10, margin: [0, 3, 0, 3], fillColor: this.s_color },
+                itemsTableInfoBlanco: { fontSize: 9, margin: [0, 0, 0, 0], fillColor: '#E3E3E3' },
+                itemsTableInfoEmpleado: { fontSize: 9, margin: [0, -1, 0, -2], fillColor: '#E3E3E3' },
+                itemsTableCentrado: { fontSize: 8, alignment: 'center' },
+                tableMargin: { margin: [0, 0, 0, 0] },
+                tableMarginEmp: { margin: [0, 15, 0, 0] },
+                tableMarginCabecera: { margin: [0, 15, 0, 0] },
+                tableMarginCabeceraEmpleado: { margin: [0, 10, 0, 0] },
+                quote: { margin: [5, -2, 0, -2], italics: true },
+                small: { fontSize: 8, color: 'blue', opacity: 0.5 }
+            }
+        };
+    }
+
+
+/*
+    // METODO PARA ESTRUCTURAR LA INFORMACION CONSULTADA EN EL PDF
+    EstructurarDatosPDF(data: any[]): Array<any> {
+        let n: any = []
+
+        let totalAuditotia = 0;
+
+
+        totalAuditotia = 0;
+        n.push({
+            style: 'tableMarginCabecera',
+            table: {
+                widths: ['*'],
+                headerRows: 1,
+                body: [
+                    [
+
+
+                        {
+                            border: [true, true, false, true],
+                            bold: true,
+                            text: 'PLATAFORMA: '+data[0][0].plataforma ,
+                            style: 'itemsTableInfo'
+                        },
+                    ]
+                ]
+            }
+        })
+
+
+        n.push({
+            style: 'tableMargin',
+            table: {
+                widths: ['auto', 'auto', 'auto', 'auto', 'auto', 'auto', '*', '*', '*', '*'],
+                headerRows: 1,
+                body: [
+                    [
+                        { text: 'ITEM', style: 'tableHeader' },
+                        { text: 'PLATAFORMA', style: 'tableHeader' },
+                        { text: 'USUARIO', style: 'tableHeader' },
+                        { text: 'IP', style: 'tableHeader' },
+                        { text: 'NOMBRE TABLA', style: 'tableHeader' },
+                        { text: 'ACCIÓN', style: 'tableHeader' },
+                        { text: 'FECHA', style: 'tableHeader' },
+                        { text: 'HORA', style: 'tableHeader' },
+                        { text: 'DATOS ORIGINALES', style: 'tableHeader' },
+                        { text: 'DATOS NUEVOS', style: 'tableHeader' }
+                    ],
+                    //  ...arr_emp.map((usu: any) => {
+                    // return
+
+                    ...data.map(audi => {
+
+                        return [
+                            { style: 'itemsTableCentrado', text: totalAuditotia + 1 },
+                            { style: 'itemsTable', text: audi.plataforma },
+                            { style: 'itemsTable', text: audi.user_name },
+                            { style: 'itemsTableCentrado', text: audi.ip_address },
+                            { style: 'itemsTableCentrado', text: audi.table_name },
+                            { style: 'itemsTable', text: audi.action },
+                            { style: 'itemsTable', text: audi.fecha_hora },
+                            { style: 'itemsTable', text: audi.fecha_hora },
+                            { style: 'itemsTable', text: audi.original_data },
+                            { style: 'itemsTable', text: audi.new_data },
+                        ]
+                    })
+
+                ]
+            },
+            layout: {
+                fillColor: function (rowIndex: any) {
+                    return (rowIndex % 2 === 0) ? '#E5E7E9' : null;
+                }
+            }
+        });
+
+        return n;
+    }
+*/
+
+    EstructurarDatosPDF(data: any[]): Array<any> {
+        let n: any = []
+    
+        let totalAuditoria = 0;
+    
+        // Añadir la cabecera con información de la plataforma
+        n.push({
+            style: 'tableMarginCabecera',
+            table: {
+                widths: ['*'],
+                headerRows: 1,
+                body: [
+                    [
+                        {
+                            border: [true, true, false, true],
+                            bold: true,
+                            text: 'PLATAFORMA: ' + data[0][0].plataforma,
+                            style: 'itemsTableInfo'
+                        },
+                    ]
+                ]
+            }
+        });
+    
+        // Añadir la tabla de datos
+        n.push({
+            style: 'tableMargin',
+            table: {
+                widths: ['auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', 'auto', '*', '*'],
+                headerRows: 1,
+                body: [
+                    [
+                        { text: 'ITEM', style: 'tableHeader' },
+                        { text: 'PLATAFORMA', style: 'tableHeader' },
+                        { text: 'USUARIO', style: 'tableHeader' },
+                        { text: 'IP', style: 'tableHeader' },
+                        { text: 'NOMBRE TABLA', style: 'tableHeader' },
+                        { text: 'ACCIÓN', style: 'tableHeader' },
+                        { text: 'FECHA', style: 'tableHeader' },
+                        { text: 'HORA', style: 'tableHeader' },
+                        { text: 'DATOS ORIGINALES', style: 'tableHeader' },
+                        { text: 'DATOS NUEVOS', style: 'tableHeader' }
+                    ],
+                    ...data.map((audi) => {
+                        totalAuditoria += 1;
+                        return [
+                            { style: 'itemsTableCentrado', text: totalAuditoria },
+                            { style: 'itemsTable', text: audi[0].plataforma },
+                            { style: 'itemsTable', text: audi[0].user_name },
+                            { style: 'itemsTableCentrado', text: audi[0].ip_address },
+                            { style: 'itemsTableCentrado', text: audi[0].table_name },
+                            { style: 'itemsTable', text: audi[0].action },
+                            { style: 'itemsTable', text:  this.getDateFromISO(audi[0].fecha_hora)  },
+                            { style: 'itemsTable', text: this.getTimeFromISO(audi[0].fecha_hora)  },
+                            { style: 'itemsTable', text: audi[0].original_data },
+                            { style: 'itemsTable', text: audi[0].new_data },
+                        ]
+                    })
+                ]
+            },
+            layout: {
+                fillColor: function (rowIndex: any) {
+                    return (rowIndex % 2 === 0) ? '#E5E7E9' : null;
+                }
+            }
+        });
+    
+        return n;
+    }
+
+     getDateFromISO(isoString: string): string {
+        const date = new Date(isoString);
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, '0');
+        const day = String(date.getDate()).padStart(2, '0');
+        return `${year}-${month}-${day}`;
+    }
+
+
+    getTimeFromISO(isoString: string): string {
+        const date = new Date(isoString);
+        const hours = String(date.getUTCHours()).padStart(2, '0');
+        const minutes = String(date.getUTCMinutes()).padStart(2, '0');
+        const seconds = String(date.getUTCSeconds()).padStart(2, '0');
+        return `${hours}:${minutes}:${seconds}`;
+    }
+
 
 
 }
