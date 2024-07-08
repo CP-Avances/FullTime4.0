e92c603de65aa032e290706d9e3a76e7f1a5532a
Juan Diego <98037053+CP-Avances@users.noreply.github.com>
2024-07-08 13:33:09 -0500

    Actualización consulta jefesdepartamento

--- servidor/src/controlador/datosGenerales/datosGeneralesControlador.ts
+++ servidor/src/controlador/datosGenerales/datosGeneralesControlador.ts
@@ -1550,6 +1550,7 @@ class DatosGeneralesControlador {
         }
     }
 
+    // TODO: ESTE METODO GENERA EL ERROR AL ELIMINAR VACACIONES
     // METODO PARA BUSCAR JEFES
     public async BuscarJefes(req: Request, res: Response): Promise<Response> {
 
@@ -1559,7 +1560,7 @@ class DatosGeneralesControlador {
         const JefesDepartamentos = await pool.query(
             `
             SELECT da.id, da.estado, n.id_departamento as id_dep, n.id_departamento_nivel, n.departamento_nombre_nivel, 
-                n.nivel, n.id_establecimiento AS id_suc, n.departamento, s.nombre AS sucursal, da.id_empl_cargo as cargo, 
+                n.nivel, n.id_sucursal AS id_suc, n.departamento, s.nombre AS sucursal, da.id_empleado_cargo as cargo, 
                 dae.id_contrato as contrato, da.id_empleado AS empleado, (dae.nombre || ' ' || dae.apellido) as fullname,
                 dae.cedula, dae.correo, c.permiso_mail, c.permiso_notificacion, c.vacacion_mail, c.vacacion_notificacion, 
                 c.hora_extra_mail, c.hora_extra_notificacion, c.comida_mail, c.comida_notificacion 
@@ -1570,7 +1571,7 @@ class DatosGeneralesControlador {
                 AND dae.id_cargo = da.id_empleado_cargo
                 AND dae.id_contrato = c.id_empleado
                 AND cg.id = $1
-                AND s.id = n.id_establecimiento
+                AND s.id = n.id_sucursal
             ORDER BY nivel ASC
             `
             ,
