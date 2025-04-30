6fce76e7c22a0ec43c66ed3a12c9efbf3414e7c0
CP-Avances <jtipan@casapazmino.com.ec>
2025-04-30 16:33:42 -0500

    actualizado rama direccJhonatan1

--- web/src/environments/environment.development.ts
+++ web/src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   //url: 'https://186.4.226.49/direccionamiento'
-  url: 'http://192.168.1.6:3005/direccionamiento'
+  url: 'http://192.168.0.148:3005/direccionamiento'
 };
