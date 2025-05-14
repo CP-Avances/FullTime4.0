0e8c949006147cf4e518604034d0ecc008a7cf52
Juan Diego <98037053+CP-Avances@users.noreply.github.com>
2025-05-14 15:12:42 -0500

    fix: corrige creación de reportes de asistencia
    Co-Authored-By: Juan Diego <68799845+juandiego-gm@users.noreply.github.com>

--- servidor/src/controlador/reportes/reportesTiempoLaboradoControlador.ts
+++ servidor/src/controlador/reportes/reportesTiempoLaboradoControlador.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import pool from '../../database';
+import { DateTime } from 'luxon';
 
 class ReportesTiempoLaboradoControlador {
 
@@ -11,6 +12,14 @@ class ReportesTiempoLaboradoControlador {
             suc.empleados = await Promise.all(suc.empleados.map(async (o: any) => {
                 const listaTimbres = await BuscarTiempoLaborado(desde, hasta, o.id);
                 o.tLaborado = await AgruparTimbres(listaTimbres);
+                await Promise.all(o.tLaborado.map(async (t: any) => {
+                    const [minAlimentacion, minLaborados, minAtrasos, minSalidasAnticipadas, minPlanificados] = await calcularTiempoLaborado(t);
+                    t.minAlimentacion = minAlimentacion;
+                    t.minLaborados = minLaborados;
+                    t.minAtrasos = minAtrasos;
+                    t.minSalidasAnticipadas = minSalidasAnticipadas;
+                    t.minPlanificados = minPlanificados;
+                }))
                 return o;
             }));
             return suc;
@@ -75,6 +84,7 @@ const AgruparTimbres = async function agruparTimbresPorClave(timbres: Timbre[]) 
                         finAlimentacion: timbresAgrupadosFecha[key][i + 2],
                         salida: i + 3 < timbresAgrupadosFecha[key].length ? timbresAgrupadosFecha[key][i + 3] : null,
                         control: timbresAgrupadosFecha[key][i].controlar_asistencia,
+                        tolerancia: timbresAgrupadosFecha[key][i].tolerancia,
                     });
                 }
                 break;
@@ -87,7 +97,7 @@ const AgruparTimbres = async function agruparTimbresPorClave(timbres: Timbre[]) 
                         entrada: timbresAgrupadosFecha[key][i],
                         salida: i + 1 < timbresAgrupadosFecha[key].length ? timbresAgrupadosFecha[key][i + 1] : null,
                         control: timbresAgrupadosFecha[key][i].controlar_asistencia,
-
+                        tolerancia: timbresAgrupadosFecha[key][i].tolerancia,
                     });
                 }
                 break;
@@ -95,6 +105,128 @@ const AgruparTimbres = async function agruparTimbresPorClave(timbres: Timbre[]) 
     }
 
     return timbresAgrupados;
+}
+
+// CONSULTAR PARAMETRO DE TOLERANCIA
+async function consultarParametroTolerancia (parametro: number) {
+    const resultado = await pool.query(
+        `
+        SELECT descripcion
+        FROM ep_detalle_parametro
+        WHERE id_parametro = $1
+        `, [parametro]
+    );
+    return resultado.rows[0]?.descripcion || null;
+}
+
+// CALCULAR DIREFERENCIA DE TIEMPO EN MINUTOS
+function calcularDiferenciaEnMinutos(fecha1: string, fecha2: string) {
+    const dt1 = DateTime.fromSQL(fecha1);
+    const dt2 = DateTime.fromSQL(fecha2);
+    const diff = dt2.diff(dt1, ['hours','minutes','seconds']);
+    return diff.as('minutes');
+}
+
+// CALCULAR TIEMPO LABORADO
+async function calcularTiempoLaborado (tLaborado: any) {
+  if (['L', 'FD'].includes(tLaborado.origen)) return [0, 0, 0, 0, 0];
+
+  const { entrada, salida, inicioAlimentacion, finAlimentacion, tolerancia, tipo } = tLaborado;
+  const parametroTolerancia = await consultarParametroTolerancia(3);
+
+  let minutosAlimentacion = 0;
+  let minutosLaborados = 0;
+  let minutosAtrasos = 0;
+  let minutosSalidasAnticipadas = 0;
+  let minutosPlanificados = 0;
+
+  const hayTimbresEntradaYSalida = entrada.fecha_hora_timbre && salida.fecha_hora_timbre;
+
+  if (entrada.fecha_hora_timbre) {
+    minutosAtrasos = calcularAtraso(
+      entrada.fecha_hora_horario,
+      entrada.fecha_hora_timbre,
+      tolerancia,
+      parametroTolerancia
+    );
+  }
+
+  if (salida.fecha_hora_timbre) {
+    minutosSalidasAnticipadas = calcularSalidaAnticipada(
+      salida.fecha_hora_timbre,
+      salida.fecha_hora_horario
+    );
+  }
+
+  if (hayTimbresEntradaYSalida) {
+    minutosLaborados = calcularDiferenciaEnMinutos(
+      entrada.fecha_hora_timbre,
+      salida.fecha_hora_timbre
+    );
+  }
+
+  minutosPlanificados = calcularDiferenciaEnMinutos(
+    entrada.fecha_hora_horario,
+    salida.fecha_hora_horario
+  );
+
+  if (tipo !== 'ES') {
+    const minAlimentacionBase = inicioAlimentacion.minutos_alimentacion;
+
+    minutosAlimentacion = calcularTiempoAlimentacion(
+      inicioAlimentacion.fecha_hora_timbre,
+      finAlimentacion.fecha_hora_timbre,
+      minAlimentacionBase
+    );
+
+    if (minutosLaborados > 0) {
+      minutosLaborados -= minutosAlimentacion;
+    }
+  }
+
+  return [
+    minutosAlimentacion,
+    minutosLaborados,
+    minutosAtrasos,
+    minutosSalidasAnticipadas,
+    minutosPlanificados,
+  ];
+};
+
+
+function calcularAtraso(
+  horaHorario: string,
+  horaTimbre: string,
+  tolerancia: number,
+  parametroTolerancia: string
+): number {
+  const diferencia = Math.max(calcularDiferenciaEnMinutos(horaHorario, horaTimbre), 0);
+
+  if (parametroTolerancia === '1') return diferencia;
+
+  if (diferencia > tolerancia) {
+    return parametroTolerancia === '2-1' ? diferencia : diferencia - tolerancia;
+  }
+
+  return 0;
+}
+
+function calcularSalidaAnticipada(
+  horaTimbre: string,
+  horaHorario: string
+): number {
+  return Math.max(calcularDiferenciaEnMinutos(horaTimbre, horaHorario), 0);
+}
+
+function calcularTiempoAlimentacion(
+  inicioTimbre: string | null,
+  finTimbre: string | null,
+  minutosPorDefecto: number
+): number {
+  if (inicioTimbre && finTimbre) {
+    return calcularDiferenciaEnMinutos(inicioTimbre, finTimbre);
+  }
+  return minutosPorDefecto;
 }
 
 interface Timbre {
@@ -108,7 +240,7 @@ interface Timbre {
     tipo_dia: string;
     estado_origen: string;
     min_alimentacion: number;
-    toleranica: number;
+    tolerancia: number;
     controlar_asistencia: boolean
 }
 
